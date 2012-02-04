c321bfe634fb94ed8dab8fd8152bec0200de3f7a
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-02-04 14:53:18 +0700

    SergeStinckwich

--- Robozzle.py
+++ Robozzle.py
@@ -1,7 +1,9 @@
 import unittest
 
-def test_true(self):
-    self.assertEquals(True, True)
+class RobozzleTest(unittest.TestCase):
 
+    def test_true(self):
+        self.assertEquals(True, True)
+        
 if __name__ == "__main__":
     unittest.main()
