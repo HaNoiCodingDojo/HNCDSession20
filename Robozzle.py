8822f3fd0dfac2bcb480d1c8cefe9438582c9950
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-02-04 15:04:47 +0700

    TBBle

--- Robozzle.py
+++ Robozzle.py
@@ -1,9 +1,17 @@
 import unittest
 
+
+
 class RobozzleTest(unittest.TestCase):
 
     def test_true(self):
         self.assertEquals(True, True)
-        
+
+    def test_0_move_0_star(self):
+        starsPosition=[,]
+        game = Game(starsPosition)
+        numberOfStarGot = game.move("")
+        self.assertEquals(len(starsPosition),numberOfStarGot)
+
 if __name__ == "__main__":
     unittest.main()
