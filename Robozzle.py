8a64b9c0833ec1c255d3de5d22f5175ce2cc75e3
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-02-04 16:03:22 +0700

    Tran

--- Robozzle.py
+++ Robozzle.py
@@ -8,14 +8,14 @@ class Game():
         if len(moves) == 0:
             return 0
         
-        if moves[0] == 'f' and self.starsPositions[0] == (1,0) and len(self.starsPositions) == 1:
+        if moves[0] == 'f' and self.starsPositions[0] == (1,0) and len(self. starsPositions) == 1:
                 return 1
            
                 
         if len(moves) == 1:
             return 0
 
-        if  moves[0] == 'f' and moves[1] == 'f' and  self.starsPositions == [(1,0),(2,0)]:
+        if  moves[0] == 'f' and moves[1] == 'f' and  self.starsPositions[0] == (1,0) and self.starsPositions[1] == (2,0) and len(self.starsPositions) == 2:
                 return 2
 
         return 0
